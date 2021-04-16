278ddca3fc94af05dfb255ff1cccde0c2d1f05ed
Iamwillcoming <2290891447@qq.com>
2021-04-16 22:07:33 +0800

    new 2021.04.16

---
+++ cc语言/lmy.c/test-4.16/test-4.16/test.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//#include <string.h>
+//void select(int arr[],int b)
+//{
+//	int maxcount=0;
+//	int count=1;
+//	int j=0;
+//	int i=0;
+//	int c=0;
+//	for(i=0;i<b-1;i++)//数组整个进行的循环
+//	{
+//		for(j=0;j<b-i;j++)//一个数与其他数的比较的循环
+//		{
+//			if(arr[i]==arr[j])
+//			{
+//				count++;
+//			}
+//			
+//		}
+//		if(count>maxcount)
+//			{
+//				maxcount=count;
+//				c=arr[i];
+//			}
+//			count=1;
+//	}
+//	printf("这个数出现的次数>:%d\n",maxcount);
+//	printf("这个数是>:%d\n",c);
+//}
+//int main()
+//{
+//	int i=0;
+//	int arr[]={1,2,3,2,2,2,5,4,2};
+//	int b=sizeof(arr)/sizeof(arr[0]);//计算数组元素个数
+//	select(arr,b);//找数字出现最多的函数
+//	printf("%d\n",b);
+//	return 0;
+//}
+
