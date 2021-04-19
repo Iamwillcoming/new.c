993d13569bbae8ce7030040cf9be249976e2f984
Iamwillcoming <2290891447@qq.com>
2021-04-19 21:22:05 +0800

    new 2021.04.19

---
+++ cc语言/lmy.c/test-4.19/test-4.19/test.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//int main()//顺时针打印数组
+//{
+//	int i=0;
+//	int j=0;
+//	int n=1;
+//	int arr[4][4]={0};
+//	for(i=0;i<4;i++)
+//	{
+//		for(j=0;j<4;j++)
+//		{
+//			arr[i][j]=n;
+//			n++;
+//			printf("%3d",arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//int sum(int a[],int sz)//数组之和
+//{
+//	int i=0;
+//	int num=0;
+//	for(i=0;i<=sz-1;i++)
+//	{
+//		num+=a[i];
+//	}
+//	return num;
+//}
+//
+//int main()
+//{
+//	int a[]={1,-2,3,10,-4,7,2,-5};
+//	int sz=sizeof(a)/sizeof(a[0]);
+//	int num=sum(a,sz);
+//	printf("%d\n",num);
+//	return 0;
+//
+//}
+
